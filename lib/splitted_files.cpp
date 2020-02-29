a611c2a3aa0e4c12240879dff8c32192c2a4005b
stryku <stryku2393@gmail.com>
2020-02-29 01:47:18 +0100

    Moved splitted_files implementation to cpp file

---
+++ lib/splitted_files.cpp
@@ -0,0 +1,42 @@
+#include "splitted_files.hpp"
+
+namespace okon {
+
+splitted_files::splitted_files(std::string_view path, std::ios::openmode mode)
+{
+  auto name = std::string{ "00" };
+
+  for (auto& output_file : m_files) {
+    output_file.open(path.data() + name, mode);
+    increment_name(name);
+  }
+}
+
+std::fstream& splitted_files::sha1_file(std::string_view sha1)
+{
+  const auto file_index = two_first_chars_to_byte(sha1.data());
+  return m_files[file_index];
+}
+
+std::fstream& splitted_files::operator[](unsigned index)
+{
+  return m_files[index];
+}
+
+void splitted_files::increment_name(std::string& current_name) const
+{
+  if (current_name[1] == '9') {
+    current_name[1] = 'A';
+  } else if (current_name[1] == 'F') {
+    current_name[1] = '0';
+
+    if (current_name[0] == '9') {
+      current_name[0] = 'A';
+    } else {
+      ++current_name[0];
+    }
+  } else {
+    ++current_name[1];
+  }
+}
+}
--- lib/splitted_files.hpp
+++ lib/splitted_files.hpp
@@ -12,26 +12,11 @@ class splitted_files
 {
 public:
   explicit splitted_files(std::string_view path,
-                          std::ios::openmode mode = std::ios::in | std::ios::out)
-  {
-    auto name = std::string{ "00" };
+                          std::ios::openmode mode = std::ios::in | std::ios::out);
 
-    for (auto& output_file : m_files) {
-      output_file.open(path.data() + name, mode);
-      increment_name(name);
-    }
-  }
+  std::fstream& sha1_file(std::string_view sha1);
 
-  std::fstream& sha1_file(std::string_view sha1)
-  {
-    const auto file_index = two_first_chars_to_byte(sha1.data());
-    return m_files[file_index];
-  }
-
-  std::fstream& operator[](unsigned index)
-  {
-    return m_files[index];
-  }
+  std::fstream& operator[](unsigned index);
 
   auto begin()
   {
@@ -44,22 +29,7 @@ public:
   }
 
 private:
-  void increment_name(std::string& current_name) const
-  {
-    if (current_name[1] == '9') {
-      current_name[1] = 'A';
-    } else if (current_name[1] == 'F') {
-      current_name[1] = '0';
-
-      if (current_name[0] == '9') {
-        current_name[0] = 'A';
-      } else {
-        ++current_name[0];
-      }
-    } else {
-      ++current_name[1];
-    }
-  }
+  void increment_name(std::string& current_name) const;
 
 private:
   std::array<std::fstream, 256u> m_files;
