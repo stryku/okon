63e02b62ab5319173e08d24935313ee1e71e059c
stryku <stryku2393@gmail.com>
2020-08-07 13:56:02 +0200

    Added BtreeSortedKeysInserter.InsertionsTillTreeHeight3AndAllFullRoot test

--- test/btree_sorted_keys_inserter_test.cpp
+++ test/btree_sorted_keys_inserter_test.cpp
@@ -377,6 +377,93 @@ TEST(BtreeSortedKeysInserter, InsertionsTillTreeHeight3AndAllRootChildren)
   EXPECT_THAT(storage.m_storage, StorageEq(expected_storage));
 }
 
+TEST(BtreeSortedKeysInserter, InsertionsTillTreeHeight3AndAllFullRoot)
+{
+  memory_storage storage;
+  btree_sorted_keys_inserter tree{ storage, k_test_order_value };
+
+  tree.insert_sorted(string_sha1_to_binary("0000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("1000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("2000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("3000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("4000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("5000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("6000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("7000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("8000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("9000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("A000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("B000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("C000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("D000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("E000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("F000000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("F100000000000000000000000000000000000000"));
+  tree.insert_sorted(string_sha1_to_binary("F200000000000000000000000000000000000000"));
+  tree.finalize_inserting();
+
+  const auto root_ptr = 4u;
+
+  const std::vector<btree_node> expected_nodes = {
+    make_node(
+      /*is_leaf=*/true, /*keys_count=*/2u,
+      { btree_node::k_unused_pointer, btree_node::k_unused_pointer, btree_node::k_unused_pointer },
+      { "0000000000000000000000000000000000000000", "1000000000000000000000000000000000000000" },
+      1u),
+    make_node(
+      /*is_leaf=*/false, /*keys_count=*/2u, { 0u, 2u, 3u },
+      { "2000000000000000000000000000000000000000", "5000000000000000000000000000000000000000" },
+      root_ptr),
+    make_node(
+      /*is_leaf=*/true, /*keys_count=*/2u,
+      { btree_node::k_unused_pointer, btree_node::k_unused_pointer, btree_node::k_unused_pointer },
+      { "3000000000000000000000000000000000000000", "4000000000000000000000000000000000000000" },
+      1u),
+    make_node(
+      /*is_leaf=*/true, /*keys_count=*/2u,
+      { btree_node::k_unused_pointer, btree_node::k_unused_pointer, btree_node::k_unused_pointer },
+      { "6000000000000000000000000000000000000000", "7000000000000000000000000000000000000000" },
+      1u),
+    make_node(
+      /*is_leaf=*/false, /*keys_count=*/2u, { 1u, 5u, 9u },
+      { "8000000000000000000000000000000000000000", "F100000000000000000000000000000000000000" },
+      btree_node::k_unused_pointer),
+    make_node(
+      /*is_leaf=*/false, /*keys_count=*/2u, { 6u, 7u, 8u },
+      { "B000000000000000000000000000000000000000", "E000000000000000000000000000000000000000" },
+      root_ptr),
+    make_node(
+      /*is_leaf=*/true, /*keys_count=*/2u,
+      { btree_node::k_unused_pointer, btree_node::k_unused_pointer, btree_node::k_unused_pointer },
+      { "9000000000000000000000000000000000000000", "A000000000000000000000000000000000000000" },
+      5u),
+    make_node(
+      /*is_leaf=*/true, /*keys_count=*/2u,
+      { btree_node::k_unused_pointer, btree_node::k_unused_pointer, btree_node::k_unused_pointer },
+      { "C000000000000000000000000000000000000000", "D000000000000000000000000000000000000000" },
+      5u),
+    make_node(
+      /*is_leaf=*/true, /*keys_count=*/1u,
+      { btree_node::k_unused_pointer, btree_node::k_unused_pointer, btree_node::k_unused_pointer },
+      { "F000000000000000000000000000000000000000", k_uninteresting_sha1 }, 5u),
+    make_node(
+      /*is_leaf=*/false, /*keys_count=*/1u, { 10u, 11u, btree_node::k_unused_pointer },
+      { "F200000000000000000000000000000000000000", k_uninteresting_sha1 }, 4u),
+    make_node(
+      /*is_leaf=*/true, /*keys_count=*/0u,
+      { btree_node::k_unused_pointer, btree_node::k_unused_pointer, btree_node::k_unused_pointer },
+      { k_uninteresting_sha1, k_uninteresting_sha1 }, 9u),
+    make_node(
+      /*is_leaf=*/true, /*keys_count=*/0u,
+      { btree_node::k_unused_pointer, btree_node::k_unused_pointer, btree_node::k_unused_pointer },
+      { k_uninteresting_sha1, k_uninteresting_sha1 }, 9u)
+  };
+
+  const auto expected_storage = to_storage(k_test_order_value, root_ptr, expected_nodes);
+
+  EXPECT_THAT(storage.m_storage, StorageEq(expected_storage));
+}
+
 TEST(BtreeSortedKeysInserter, Order3_InsertionsTillTreeHeight3)
 {
   constexpr auto tree_order{ 3u };
