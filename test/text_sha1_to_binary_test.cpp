d3f30466479e5f06cfa283df1f2cfd42ae75a576
stryku <stryku2393@gmail.com>
2020-10-08 22:38:03 +0200

    Divided sha1 to binary test for string and simd test

--- test/text_sha1_to_binary_test.cpp
+++ test/text_sha1_to_binary_test.cpp
@@ -15,8 +15,6 @@ struct TextSha1ToBinaryState
   sha1_t expected;
 };
 
-using TextSha1ToBinaryTest = testing::TestWithParam<TextSha1ToBinaryState>;
-
 TextSha1ToBinaryState values[] = {
   { "0000000000000000000000000000000000000000 --------------------"sv,
     sha1_t{ 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u, 0u } },
@@ -28,12 +26,25 @@ TextSha1ToBinaryState values[] = {
             0xBA, 0x98, 0x76, 0x54, 0x32, 0x10, 0x01, 0x23, 0x45, 0x67 } }
 };
 
-TEST_P(TextSha1ToBinaryTest, ProducesCorrectBinary)
+using StringSha1ToBinaryTest = testing::TestWithParam<TextSha1ToBinaryState>;
+
+TEST_P(StringSha1ToBinaryTest, ProducesCorrectBinary)
 {
   const auto& [text_sha1, expected] = GetParam();
-  const auto result = text_sha1_to_binary(text_sha1.data());
+  const auto result = details::string_sha1_to_binary(text_sha1.data());
   EXPECT_THAT(result, Eq(expected));
 }
 
-INSTANTIATE_TEST_SUITE_P(TextSha1ToBinary, TextSha1ToBinaryTest, testing::ValuesIn(values));
+INSTANTIATE_TEST_SUITE_P(TextSha1ToBinary, StringSha1ToBinaryTest, testing::ValuesIn(values));
+
+using SIMDSha1ToBinaryTest = testing::TestWithParam<TextSha1ToBinaryState>;
+
+TEST_P(SIMDSha1ToBinaryTest, ProducesCorrectBinary)
+{
+  const auto& [text_sha1, expected] = GetParam();
+  const auto result = details::simd_string_sha1_to_binary(text_sha1.data());
+  EXPECT_THAT(result, Eq(expected));
+}
+
+INSTANTIATE_TEST_SUITE_P(TextSha1ToBinary, SIMDSha1ToBinaryTest, testing::ValuesIn(values));
 }
